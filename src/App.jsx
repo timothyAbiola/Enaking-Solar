346bf881c4ed44005f33769c20fdd7195e6347e5
Abiola Timothy <abiolatimothy75@gmail.com>
2023-12-03 18:50:20 +0100

    added loading spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Landing from '../pages/Landing';
 import Offer from '../components/Offer';
+import spinner from '../src/assets/spinner1.png'
 
 function App() {
+  const [loading, setloading] = useState(false)
+  useEffect(() =>{
+    setloading(true)
+    setTimeout(()=> {
+      setloading(false)
+    }, 3000)
+  }, [])
  
   return (
     <>
-      <Routes>
+    {
+      // need to change spinner image with a package from npm
+      loading ? <div class="loader"></div>
+    : 
+    <Routes>
         <Route path="/" element={<Landing />}></Route>
         <Route path='/service' element={<Offer/>}/>
-      </Routes>
+    </Routes>
+    }
+      
     </>
   );
 }
